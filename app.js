5a3b8a88765a868284de15aff7b4632893c8b808
Stephane Lefevre <stephane.lefevre@public-i.info>
2020-10-04 19:21:09 +0100

    adding api for playing audio on mpd (via bluetooth)

--- app.js
+++ app.js
@@ -3,39 +3,72 @@ const app = express()
 const port = 3000
 const {Device} = require('ps4-waker');
 const wol = require('node-wol');
+const { exec } = require('child_process');
+
+function checkIP(req, res) {
+  var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  if (!ip.includes('192.168.0.'))
+  {
+    res.send("local only");
+    return false;
+  }
+  return true;
+}
 
 app.get('/ps4', (req, res) => {
-
-var ps4 = new Device();
-var promise = ps4.turnOn();
-promise.then(() => {
-  console.log('ps4-waker');
-  res.send('PS4 woken up!');
-  ps4.close()
-}).catch((e) => {
-  res.send(`ps4-waker: ${e}`);
-  console.log(`ps4-waker: ${e}`)
-  ps4.close()
-});
-
+if (checkIP(req, res)) {
+  var ps4 = new Device({
+          bindAddress: "192.168.0.110"
+      }
+  );
+  var promise = ps4.turnOn();
+  promise.then(() => {
+    console.log('ps4-waker');
+    res.send('PS4 woken up!');
+    ps4.close()
+  }).catch((e) => {
+    res.send(`ps4-waker: ${e}`);
+    console.log(`ps4-waker: ${e}`)
+    ps4.close()
+  });
+}
 });
 
 app.get('/pc', (req, res) => {
-  wol.wake('BC:5F:F4:CE:D8:CD', {
-  address: '192.168.0.255',
-  port: 9
-}, function(error) {
-  if(error) {
-    // handle error
-    console.console.log(error);
-    res.send(error);
-    return;
-  }
-  else{
-    console.log('pc-waker');
-    res.send('Pc woken up!');
-  }
+  if (checkIP(req, res)) {
+    wol.wake('BC:5F:F4:CE:D8:CD', {
+    address: '192.168.0.255',
+    port: 9
+  }, function(error) {
+    if(error) {
+      // handle error
+      console.log(error);
+      res.send(error);
+      return;
+    }
+    else{
+      console.log('pc-waker');
+      res.send('Pc woken up!');
+    }
+  });
+}
 });
+
+app.get('/playNova', (req, res) => {
+  //
+  exec('echo -e \"connect 54:60:09:6F:13:4B\nquit\n\" | bluetoothctl && sleep 2 && mpc play', (err, stdout, stderr) => {
+  if (err) {
+    //some err occurred
+    console.error(err)
+    res.send(error);
+  } else {
+   // the *entire* stdout and stderr (buffered)
+   console.log(`stdout: ${stdout}`);
+   console.log(`stderr: ${stderr}`);
+   res.send("Playing nova");
+   console.log("Playing nova");
+  }
+  });
 
 });
 
---
+++ mpd_bluetooth_google_home.md
@@ -0,0 +1,46 @@
+https://gist.github.com/actuino/9548329d1bba6663a63886067af5e4cb
+
+sudo apt-get install pulseaudio pulseaudio-module-bluetooth
+
+sudo usermod -G bluetooth -a pi
+
+pulseaudio --start
+
+bluetoothctl -- connect 54:60:09:6F:13:4B
+
+edit /etc/pulse/default.pa , add
+load-module module-switch-on-connect
+load-module module-native-protocol-tcp auth-ip-acl=127.0.0.1
+
+
+sudo apt-get install mpd mpc
+sudo adduser mpd pulse
+sudo adduser mpd pulse-access
+
+edit /etc/mpd.conf
+bind_to_address         "/run/mpd/socket"
+#comment out alsa
+audio_output {
+        type            "pulse"
+        name            "My Pulse Output"
+        server          "127.0.0.1"             # optional
+#       sink            "remote_server_sink"    # optional
+}
+
+edit /etc/dbus-1/system.d/bluetooth.conf ?
+<policy user="pi">
+   <allow send_destination="org.bluez"/>
+   <allow send_interface="org.bluez.Agent1"/>
+   <allow send_interface="org.bluez.MediaEndpoint1"/>
+   <allow send_interface="org.bluez.MediaPlayer1"/>
+   <allow send_interface="org.bluez.GattCharacteristic1"/>
+   <allow send_interface="org.bluez.GattDescriptor1"/>
+   <allow send_interface="org.freedesktop.DBus.ObjectManager"/>
+   <allow send_interface="org.freedesktop.DBus.Properties"/>
+ </policy>
+
+echo -e "connect 54:60:09:6F:13:4B\nquit\n" | bluetoothctl
+echo -e "disconnect 54:60:09:6F:13:4B\nquit\n" | bluetoothctl
+
+ mpc add http://radionova.ice.infomaniak.ch/radionova-high.mp3
+ mpc play
